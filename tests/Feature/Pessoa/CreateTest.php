2681aefc1a33331c4d5745f7ed2810fe105c82cb
Thiago Rodrigues <hey@thiagorodrigues.dev>
2023-10-27 19:38:52 -0300

    test: improve create tests

--- tests/Feature/Pessoa/CreateTest.php
+++ tests/Feature/Pessoa/CreateTest.php
@@ -6,19 +6,17 @@ use function Pest\Laravel\postJson;
 
 describe('Validation', function () {
     it('should return 422 when apelido validation fail', function () {
-        $pessoa          = Pessoa::factory()->makeOne();
-        $pessoa->apelido = null;
+        $pessoa = Pessoa::factory(['apelido' => null])->raw();
 
-        postJson(route('pessoas.store', $pessoa->toArray()))
+        postJson(route('pessoas.store', $pessoa))
             ->assertUnprocessable()
             ->assertInvalid(['apelido']);
     });
 
     it('should return 422 when apelido has more than 32 characters', function () {
-        $pessoa          = Pessoa::factory()->makeOne();
-        $pessoa->apelido = str_repeat('a', 33);
+        $pessoa = Pessoa::factory(['apelido' => str_repeat('a', 33)])->raw();
 
-        postJson(route('pessoas.store', $pessoa->toArray()))
+        postJson(route('pessoas.store', $pessoa))
             ->assertUnprocessable()
             ->assertInvalid(['apelido']);
     });
@@ -32,55 +30,50 @@ describe('Validation', function () {
     });
 
     it('should return 422 when nome validation fail', function () {
-        $pessoa       = Pessoa::factory()->makeOne();
-        $pessoa->nome = null;
+        $pessoa = Pessoa::factory(['nome' => null])->raw();
 
-        postJson(route('pessoas.store', $pessoa->toArray()))
+        postJson(route('pessoas.store', $pessoa))
             ->assertUnprocessable()
             ->assertInvalid('nome');
     });
 
     it('should return 422 when nome has more than 100 characters', function () {
-        $pessoa       = Pessoa::factory()->makeOne();
-        $pessoa->nome = str_repeat("a", 101);
+        $pessoa = Pessoa::factory(['nome' => str_repeat("a", 101)])->raw();
 
-        postJson(route('pessoas.store', $pessoa->toArray()))
+        postJson(route('pessoas.store', $pessoa))
             ->assertUnprocessable()
             ->assertInvalid(['nome']);
     });
 
     it('should return 422 when nascimento validation fail', function () {
-        $pessoa             = Pessoa::factory()->makeOne();
-        $pessoa->nascimento = null;
+        $pessoa = Pessoa::factory(['nascimento' => null])->raw();
 
-        postJson(route('pessoas.store', $pessoa->toArray()))
+        postJson(route('pessoas.store', $pessoa))
             ->assertUnprocessable()
             ->assertInvalid(['nascimento']);
     });
 
     it('should return 422 when invalid date (nascimento) was provided validation fail', function () {
-        $pessoa             = Pessoa::factory()->makeOne();
-        $pessoa->nascimento = "2023-02-30";
+        $pessoa = Pessoa::factory(['nascimento' => '2023-02-30'])->raw();
 
-        postJson(route('pessoas.store', $pessoa->toArray()))
+        postJson(route('pessoas.store', $pessoa))
             ->assertUnprocessable()
             ->assertInvalid(['nascimento']);
     });
 
     it('should return 422 when stack validation fail', function () {
-        $pessoa        = Pessoa::factory()->makeOne();
-        $pessoa->stack = ["", "Valid"];
+        $pessoa = Pessoa::factory(['stack' => ["", "Valid"]])->raw();
 
-        postJson(route('pessoas.store', $pessoa->toArray()))
+        postJson(route('pessoas.store', $pessoa))
             ->assertUnprocessable()
             ->assertInvalid(['stack.0']);
     });
 });
 
 it('should be able to store a new person when data is valid', function () {
-    $pessoa = Pessoa::factory()->makeOne();
+    $pessoa = Pessoa::factory()->raw();
 
-    postJson(route('pessoas.store', $pessoa->toArray()))
+    postJson(route('pessoas.store', $pessoa))
         ->assertCreated()
         ->assertHeader('Location')
         ->assertJsonStructure([
